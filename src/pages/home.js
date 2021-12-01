f4558b39905079ebe6a8ece764bc6d603b7c7011
Francisco García <francoffee57@gmail.com>
2021-12-01 12:14:33 -0600

    Termine el manejo de rutas del proyecto

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,31 @@
-const Home = () => {
-    const view = `
-      <div class="Characters">
-        <article class="Character-item">
-          <a href="#/1/">
-            <img src="image" alt="name">
-            <h2>Name</h2>
-          </a>
-        </article>
-      </div>
-    `;
-    return view;
-  };
+// const Home = () => {
+//     const view = `
+//       <div class="Characters">
+//         <article class="Character-item">
+//           <a href="#/1/">
+//             <img src="image" alt="name">
+//             <h2>Name</h2>
+//           </a>
+//         </article>
+//       </div>
+//     `;
+//     return view;
+//   };
   
-  export default Home;
+//   export default Home;
+
+const Home = () => {
+  const view = `
+    <div class="Characters">
+      <article class="Character-item">
+        <a href="#/1/">
+          <img src="image" alt="name">
+          <h2>Name</h2>
+        </a>
+      </article>
+    </div>
+  `;
+  return view;
+};
+
+export default Home;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import Character from "../pages/Character"
 import Error404 from "../pages/Error404"
 import Home from "../pages/Home"
 
+import getHash from "../utils/getHash"
+import resolveRoutes from "../utils/resolveRoutes"
+
+
 const routes = {
     '/': Home,
     '/:id': Character,
@@ -11,10 +15,14 @@ const routes = {
 
 //Manejador de nustras rutas
 const router = async () => {
-    const header = null || document.getElementById('header')
-    const content = null || document.getElementById('content')
-
-    header.innerHTML = await Header()
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+  
+    header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
 }
 
 export default router
