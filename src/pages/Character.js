f778209ff530c5eb7dff45feed755abb8d328712
Francisco García <francoffee57@gmail.com>
2021-12-01 18:52:40 -0600

    Agregué un poco mas de información de los personajes

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -18,16 +18,27 @@ const Character = async () => {
     return Error404
   }
 
+  const series =characterView.series.items.map(item => {
+    return `<li>${item.name}</li>`
+  }).join('')
 
+  const comics =characterView.comics.items.map(item => {
+    return `<li>${item.name}</li>`
+  }).join('')
 
   const view = `
       <div class="Characters-inner">
         <article class="Characters-card">
           <img src="${characterView.thumbnail.path}.${characterView.thumbnail.extension}" alt="${characterView.name}">
           <h2>${characterView.name}</h2>
+          <p class="Characters-description">${characterView.description}</p>
         </article>
         <article class="Characters__card">
-          <p class="Characters-description">${characterView.description}</p>
+          <h3>Series: </h3>
+          <ul>${series}</ul>
+          <h3>Comics:</h3>
+          <ul>${comics}</ul>
+          <a href="${characterView.urls[1].url}" target="_blank" >wiki</a>
       </div>
     `;
     return view;
