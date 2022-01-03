102161a856f7171d026e040ddb1bf5db0a5dc89c
Francisco García <francoffee57@gmail.com>
2022-01-03 12:34:59 -0600

    Corrección de la imagen del footer y arregle la card

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <li  class="footer__link__li"><a target="_blank" href="https://github.com/frangardev/SPA-Marvel" class="footer--a">Repositorio</a></li>
         </ul class="footer__link footer__link--">
         <a target="_blank" href="https://frangardev.github.io/" class="footer__hecho-por">
-            <img class="footer__img" src="https://frangardev.github.io/asets/img/frangardev-pixelart.png" alt="Dibujo pixel art de frangardev">
+            <img class="footer__img" src="https://frangardev.github.io/redes/assets/img/frangardev-pixelart.png" alt="Dibujo pixel art de frangardev">
         <a/>
         <ul class="footer__link footer__link--right"> 
             <li  class="footer__link__li"><a target="_blank" href="https://www.linkedin.com/in/frangardev/" class="footer--a">Linkedin</a></li>
